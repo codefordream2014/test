aad893adca1f59e68976a0e85a8fa3dafd771bb6
zhangzhaoyang <zhangzhaoyang@gome.com.cn>
2016-06-28 20:39:43 +0800

    init

---
+++ src/main/java/com/gome/main/Main.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.gome.main;
+
+import java.util.Date;
+
+/**
+ * @author zhangzhaoyang
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	/*	java.util.Date utilDate=new Date();
+		
+		java.sql.Timestamp stp=new java.sql.Timestamp(utilDate.getTime());
+		
+		System.out.println(stp);*/
+		
+		int[] arr = new int[]{8,2,1,0,3};
+		int[] index = new int[] {2,0,3,2,4,0,1,3,2,3,3};
+		String tel="";
+		for(int i : index){
+			tel+=arr[i];
+		}
+		
+		System.out.println("联系方式"+tel);
+		
+	}
+
+}
